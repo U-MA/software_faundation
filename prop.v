abcc887ee9ce83ca4913c02b9fab44a681f20ae6
U-MA <g.0224incident@gmail.com>
2016-05-29 12:55:18 +0900

    Prop.vの最後まで読んだ

--- prop.v
+++ prop.v
@@ -440,3 +440,15 @@ Proof.
     apply ev_O.
   Case "n = S n'".
 Admitted.
+
+Theorem plus_assoc':forall n m p:nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p.
+  induction n as [|n'].
+  Case "n = 0".
+    reflexivity.
+  Case "n = S n'".
+    simpl.
+    rewrite -> IHn'.
+    reflexivity. Qed.
