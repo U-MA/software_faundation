c8b91244438ed1989b87cf6c04ebb2869ee99a6d
U-MA <g.0224incident@gmail.com>
2016-06-01 21:25:42 +0900

    偽であるということの前まで

--- logic.v
+++ logic.v
@@ -136,3 +136,139 @@ Proof.
   apply H0.
   apply H1.
   apply R'. Qed.
+
+Definition MyProp_iff_ev : forall n, MyProp n <-> ev n :=
+  (fun (n : nat) => conj (MyProp n -> ev n) (ev n-> MyProp n)
+    (ev_MyProp n) (MyProp_ev n)).
+
+Inductive or (P Q : Prop) : Prop :=
+|or_introl : P -> or P Q
+|or_intror : Q -> or P Q.
+
+Notation "P \/ Q" := (or P Q) : type_scope.
+
+Theorem or_commut : forall P Q : Prop,
+  P \/ Q -> Q \/ P.
+Proof.
+  intros P Q H.
+  inversion H as [HP | HQ].
+  apply or_intror.
+  apply HP.
+  apply or_introl.
+  apply HQ. Qed.
+
+Theorem or_commut' : forall P Q : Prop,
+  P \/ Q -> Q \/ P.
+Proof.
+  intros P Q H.
+  inversion H as [HP | HQ].
+  right. apply HP.
+  left. apply HQ. Qed.
+
+Theorem or_distributes_over_and_1 : forall P Q R : Prop,
+  P \/ (Q /\ R) -> (P \/ Q) /\ (P \/ R).
+Proof.
+  intros P Q R H.
+  inversion H as [HP | [HQ HR]].
+  split.
+  left. apply HP.
+  left. apply HP.
+  split.
+  right. apply HQ.
+  right. apply HR. Qed.
+
+Theorem or_distributes_over_and_2 : forall P Q R : Prop,
+  (P \/ Q) /\ (P \/ R) -> P \/ (Q /\ R).
+Proof.
+  intros P Q R H.
+  inversion H as [[HP | HQ] [HP' | HR]].
+  left. apply HP.
+  left. apply HP.
+  left. apply HP'.
+  right.
+  split.
+  apply HQ.
+  apply HR. Qed.
+
+Theorem or_distributes_over_and : forall P Q R : Prop,
+  P \/ (Q /\ R) <-> (P \/ Q) /\ (P \/ R).
+Proof.
+  intros P Q R.
+  split.
+  intros H.
+  inversion H.
+  split.
+  left. apply H0.
+  left. apply H0.
+  inversion H0 as [HQ HR].
+  split.
+  right. apply HQ.
+  right. apply HR.
+  intros H.
+  inversion H as [[HP | HQ] [HP' | HR]].
+  left. apply HP.
+  left. apply HP.
+  left. apply HP'.
+  right.
+  split.
+  apply HQ.
+  apply HR. Qed.
+
+Theorem andb_true__and : forall b c,
+  andb b c = true -> b = true /\ c = true.
+Proof.
+  intros b c H.
+  destruct b.
+    destruct c.
+      apply conj.
+      reflexivity.
+      reflexivity.
+    inversion H.
+    inversion H. Qed.
+
+Theorem and__andb_true : forall b c,
+  b = true /\ c = true -> andb b c = true.
+Proof.
+  intros b c H.
+  inversion H.
+  rewrite H0.
+  rewrite H1.
+  reflexivity. Qed.
+
+Theorem andb_false : forall b c,
+  andb b c = false -> b = false \/ c = false.
+Proof.
+  intros b c H.
+  destruct b.
+    destruct c.
+    inversion H.
+    right.
+    reflexivity.
+    left.
+    reflexivity. Qed.
+
+Theorem orb_true : forall b c,
+  orb b c = true -> b = true \/ c = true.
+Proof.
+  intros b c H.
+  destruct b.
+  left.
+  reflexivity.
+  destruct c.
+  right.
+  reflexivity.
+  inversion H. Qed.
+
+Theorem orb_false : forall b c,
+  orb b c = false -> b = false /\ c = false.
+Proof.
+  intros b c H.
+  destruct b.
+    destruct c.
+    inversion H.
+    inversion H.
+  split.
+  reflexivity.
+  destruct c.
+  inversion H.
+  reflexivity. Qed.
