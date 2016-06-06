e8aead987114e96680f758aff3a1c18820899aa4
U-MA <g.0224incident@gmail.com>
2016-06-06 22:17:25 +0900

    optimize_Oplus_soundの途中まで

---
+++ imp.v
@@ -0,0 +1,92 @@
+Require Export SfLib_J.
+
+Module AExp.
+
+Inductive aexp : Type :=
+|ANum : nat -> aexp
+|APlus : aexp -> aexp -> aexp
+|AMinus : aexp -> aexp -> aexp
+|AMult : aexp -> aexp -> aexp.
+
+Inductive bexp : Type :=
+|BTrue : bexp
+|BFalse : bexp
+|BEq : aexp -> aexp -> bexp
+|BLe : aexp -> aexp -> bexp
+|BNot : bexp -> bexp
+|BAnd : bexp -> bexp -> bexp.
+
+Fixpoint aeval (e: aexp) : nat :=
+  match e with
+  |ANum n => n
+  |APlus a1 a2 => (aeval a1) + (aeval a2)
+  |AMinus a1 a2 => (aeval a1) - (aeval a2)
+  |AMult a1 a2 => (aeval a1) * (aeval a2)
+  end.
+
+Example test_aeval1 :
+  aeval (APlus (ANum 2) (ANum 2)) = 4.
+Proof.
+  reflexivity. Qed.
+
+Fixpoint beval (e: bexp) : bool :=
+  match e with
+  |BTrue => true
+  |BFalse => false
+  |BEq a1 a2 => beq_nat (aeval a1) (aeval a2)
+  |BLe a1 a2 => ble_nat (aeval a1) (aeval a2)
+  |BNot b1 => negb (beval b1)
+  |BAnd b1 b2 => andb (beval b1) (beval b2)
+  end.
+
+Fixpoint optimize_Oplus (e: aexp) : aexp :=
+  match e with
+  |ANum n => ANum n
+  |APlus (ANum O) e2 => optimize_Oplus e2
+  |APlus e1 e2 =>
+      APlus (optimize_Oplus e1) (optimize_Oplus e2)
+  |AMinus e1 e2 =>
+      AMinus (optimize_Oplus e1) (optimize_Oplus e2)
+  |AMult e1 e2 =>
+      AMult (optimize_Oplus e1) (optimize_Oplus e2)
+  end.
+
+Example test_optimize_Oplus :
+  optimize_Oplus (APlus (ANum 2)
+    (APlus (ANum 0)
+      (APlus (ANum 0) (ANum 1))))
+  = APlus (ANum 2) (ANum 1).
+Proof. reflexivity. Qed.
+
+Theorem optimize_Oplus_sound : forall e,
+  aeval (optimize_Oplus e) = aeval e.
+Proof.
+  intros e.
+  induction e.
+  Case "ANum".
+    reflexivity.
+  Case "APlus".
+    destruct e1.
+    SCase "e1 = ANum n".
+      destruct n.
+      SSCase "n = 0".
+        simpl.
+        apply IHe2.
+      SSCase "n <> 0".
+        simpl.
+        rewrite IHe2.
+        reflexivity.
+    SCase "e1 = APlus e1_1 e1_2".
+      simpl.
+      simpl in IHe1.
+      rewrite IHe1.
+      rewrite IHe2.
+      reflexivity.
+    SCase "e1 = AMult e1_1 e1_2".
+      simpl.
+      simpl in IHe1.
+      rewrite IHe1.
+      rewrite IHe2.
+      reflexivity.
+  Case "AMinus".
+Admitted.
