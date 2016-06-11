a005d2c745b51e634dfceb644f89f9a7fac6ab43
U-MA <g.0224incident@gmail.com>
2016-06-12 07:48:04 +0900

    test_evalまで

--- imp.v
+++ imp.v
@@ -589,3 +589,70 @@ Fixpoint ceval_step2 (st:state) (c:com) (i:nat) : state :=
             else st
       end
   end.
+
+Fixpoint ceval_step3 (st:state) (c:com) (i:nat)
+  : option state :=
+  match i with
+  | O => None
+  | S i' =>
+      match c with
+      | SKIP  =>
+          Some st
+      | l ::= a1 =>
+          Some (update st l (aeval st a1))
+      | c1 ; c2 =>
+          match (ceval_step3 st c1 i') with
+          | Some st' => ceval_step3 st' c2 i'
+          | None => None
+          end
+      | IFB b THEN c1 ELSE c2 FI =>
+          if (beval st b)
+            then ceval_step3 st c1 i'
+            else ceval_step3 st c2 i'
+      | WHILE b1 DO c1 END =>
+          if (beval st b1)
+            then match (ceval_step3 st c1 i') with
+            | Some st' => ceval_step3 st' c i'
+            | None => None
+            end
+          else Some st
+      end
+  end.
+
+Notation "'LETOPT' x <== e1 'IN' e2"
+  := (match e1 with
+  | Some x => e2
+  | None => None
+  end)
+  (right associativity, at level 60).
+
+Fixpoint ceval_step (st:state) (c:com) (i:nat)
+  : option state :=
+  match i with
+  | O => None
+  | S i' =>
+      match c with
+      | SKIP =>
+          Some st
+      | l ::= a1 =>
+          Some (update st l (aeval st a1))
+      | c1 ; c2 =>
+          LETOPT st' <== ceval_step st c1 i' IN
+          ceval_step st' c2 i'
+      | IFB b THEN c1 ELSE c2 FI =>
+          if (beval st b)
+            then ceval_step st c1 i'
+            else ceval_step st c2 i'
+      | WHILE b1 DO c1 END =>
+          if (beval st b1)
+            then LETOPT st' <== ceval_step st c1 i' IN
+              ceval_step st' c i'
+            else Some st
+      end
+  end.
+
+Definition test_ceval (st:state) (c:com) :=
+  match ceval_step st c 500 with
+  | None => None
+  | Some st => Some (st X, st Y, st Z)
+  end.
